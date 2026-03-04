5ed7267ebc4749e0dda54cb42f0067a5a3907374
unyksunny <unyksunny@gmail.com>
2026-03-04 08:16:06 +0530

    safe area fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,11 @@
 import { Stack } from 'expo-router';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '../global.css';
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: 'Home', headerShown: true }} />
-        <Stack.Screen name="demo" options={{ title: 'Demo', headerShown: true }} />
-      </Stack>
-    </SafeAreaProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home', headerShown: true }} />
+      <Stack.Screen name="demo" options={{ title: 'Demo', headerShown: true }} />
+    </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
 import { router } from 'expo-router';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   return (
-    <View>
+    <SafeAreaView>
       <Text className="text-4xl font-bold text-blue-500">Home Screen</Text>
       <TouchableOpacity
         className="items-center rounded-xl bg-azure-600 py-3"
         onPress={() => router.push('/demo')}>
         <Text className="font-semibold text-white">Theme Page</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 }
